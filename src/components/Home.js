4b96e156b55d69e88fd071278d04316a0b10bfda
Lydeon <leonlee190@gmail.com>
2021-06-09 09:22:29 -0700

    Playing with some fonts

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <p className="welcome">
+        안녕하세요 처음 뵙겠습니다. <br />
+        Hello Nice to meet you
+      </p>
+    </div>
+  );
+}
+
+export default Home;
